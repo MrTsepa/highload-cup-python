ef641610855865def75db68ef2c079fb1e0d4a3e
MrTsepa <tsepa.stas@gmail.com>
2017-08-17 11:17:24 +0300

    user_visits finished

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
 from bson.json_util import dumps
 import bottle
+import pymongo
 
 from utils import fill_db2
 from bottle.ext.mongo import MongoPlugin
@@ -12,32 +12,15 @@ app.install(plugin)
 
 @app.get('/users/<id:int>')
 def get_user(id, mongodb):
-    user = mongodb.users.find_one({'id': id})
+    user = mongodb.users.find_one({'_id': id}, {'_id': False})
     if user:
         return user
     return bottle.HTTPError(404)
 
 
-@app.get('/users/<id:int>/visits')
-def get_user_visits(id, mongodb):
-    fromDate = bottle.request.query.fromDate
-    toDate = bottle.request.query.toDate
-    country = bottle.request.query.country
-    toDistance = bottle.request.query.toDistance
-    filter_query = {'user': id}
-    if len(fromDate) > 0:
-        filter_query['visited_at']['$gt'] = int(fromDate)
-    if len(toDate) > 0:
-        filter_query['visited_at']['$lt'] = int(toDate)
-    if len(country) > 0:
-        filter_query['country'] = country
-    result = mongodb.visits.find({'user': id}, {'_id': False})
-    return dumps({'visits': result})
-
-
 @app.get('/locations/<id:int>')
 def get_location(id, mongodb):
-    location = mongodb.locations.find_one({'id': id})
+    location = mongodb.locations.find_one({'_id': id}, {'_id': False})
     if location:
         return location
     return bottle.HTTPError(404)
@@ -45,10 +28,40 @@ def get_location(id, mongodb):
 
 @app.get('/visits/<id:int>')
 def get_visit(id, mongodb):
-    visits = mongodb.visits.find_one({'id': id})  # TODO exclude
+    visits = mongodb.visits.find_one({'_id': id}, {'_id': False})
     if visits:
         return visits
     return bottle.HTTPError(404)
+
+
+@app.get('/users/<id:int>/visits')
+def get_user_visits(id, mongodb):
+    if mongodb.users.find({'_id': id}, {'_id': 1}).limit(1).count(with_limit_and_skip=True) == 0:
+        return bottle.HTTPError(404)
+    fromDate = bottle.request.query.fromDate
+    toDate = bottle.request.query.toDate
+    country = bottle.request.query.country
+    toDistance = bottle.request.query.toDistance
+    filter_query = {'user': id}
+    if fromDate:
+        if 'visited_at' not in filter_query:
+            filter_query['visited_at'] = {}
+        filter_query['visited_at']['$gt'] = int(fromDate)
+    if toDate:
+        if 'visited_at' not in filter_query:
+            filter_query['visited_at'] = {}
+        filter_query['visited_at']['$lt'] = int(toDate)
+    if country:
+        filter_query['location__country'] = country
+    if toDistance:
+        if 'location__distance' not in filter_query:
+            filter_query['location__distance'] = {}
+        filter_query['location__distance']['$lt'] = int(toDistance)
+    result = mongodb.visits.find(
+        filter_query,
+        {'_id': False, 'id': False, 'user': False, 'location__country': False, 'location__distance': False}
+    ).sort('visited_at', pymongo.ASCENDING)
+    return dumps({'visits': result})
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -12,17 +12,23 @@ def fill_db2():
     db.visits.delete_many({})
 
     users_data = json.load(open(PATH_TO_DATA + '/users_1.json', 'rb'))
-    db.users.insert_many(users_data['users'])
-
     locations_data = json.load(open(PATH_TO_DATA + '/locations_1.json', 'rb'))
+    visits_data = json.load(open(PATH_TO_DATA + '/visits_1.json', 'rb'))
+
+    for user in users_data['users']:
+        user['_id'] = user['id']
+    db.users.insert_many((users_data['users']))
+
+    for location in locations_data['locations']:
+        location['_id'] = location['id']
     db.locations.insert_many(locations_data['locations'])
 
-    visits_data = json.load(open(PATH_TO_DATA + '/visits_1.json', 'rb'))
     for visit in visits_data['visits']:
-        location = db.locations.find_one({'id': visit['location']})
-        visit['country'] = location['country']
-        visit['distance'] = location['distance']
-        db.visits.insert_one(visit)
+        visit['_id'] = visit['id']
+        location = db.locations.find_one({'_id': visit['location']})
+        visit['location__country'] = location['country']
+        visit['location__distance'] = location['distance']
+    db.visits.insert_many(visits_data['visits'])
 
 
 if __name__ == '__main__':
